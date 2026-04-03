da27b3435a0428711b8f9c4491ded6a1b04fe630
dreaminalgodevelopmement-cmyk <dreaminalgodevelopmement@gmail.com>
2026-04-03 23:32:36 +0530

    title done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Upgrade from "@/components/Home/upgrade";
 import Perks from "@/components/Home/perks";
 import { Metadata } from "next";
 export const metadata: Metadata = {
-  title: "CrypGo",
+  title: "Dreamin",
 };
 
 export default function Home() {
